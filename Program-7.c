044f6c1d9271646abedd4e672c393c3d60526e6c
Abhishek L <84949557+codingboy03@users.noreply.github.com>
2021-06-19 20:26:17 +0530

    Update Program-7.c
    for loop

--- Program-7.c
+++ Program-7.c
@@ -9,10 +9,12 @@
 	  printf("Enter size of the array and the array elements");
 	  scanf("%d",&n);
 	  
-	  for(i=0;i<n;i++)
-	  scanf("%d",&a[]);//missing code
+	  for(i=0;i<n;i++){
+	  scanf("%d",&a[i]);
+    }//missing code
 	  printf("Array elements in reverse order : ");
-	  for(i=;i;i) //missing code
+	  for(i=n-1;i>=0;--i){ //missing code
 	  printf("%d ",a[i]);
-	  return(0);
+    }
+    return(0);
 	}
